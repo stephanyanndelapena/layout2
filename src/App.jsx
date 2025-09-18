abb14b4b8a58a5333a8440420418bfef0fe9c32a
stephanyanndelapena <delapenastephanyann@gmail.com>
2025-09-18 19:26:25 +0800

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-
+function OuterGreen() {
   return (
-<div className="outer-green">
-      <div className="blue-box-top">
-        <div className="row">
-          <div className="peach-box"></div>
-
-          <div className="peach-box">
-            <div className="red-box">
-              <div className="pink-left"></div>
-              <div className="pink-right"></div>
-            </div>
-          </div>
-
-          <div className="peach-box"></div>
-        </div>
-      </div>
-
+    <div className="outer-green">
+      <BlueBoxtop />
       <div className="middle-row">
         <div className="big-pink"></div>
         <div className="right-col">
@@ -29,16 +14,89 @@ function App() {
           <div className="small-pink"></div>
         </div>
       </div>
-
-      <div className="blue-box-bot">
-        <div className="row1">
-          <div className="purple-box"></div>
-          <div className="small-peach"></div>
-          <div className="small-peach"></div>
-          <div className="purple-box"></div>
-        </div>
-      </div>
+      <BlueBoxBottom />
     </div>
+  )
+}
+function BlueBoxtop() {
+  return (
+    <div className="blue-box-top">
+      <Row />
+    </div>
+  )
+}
+function Row() {
+  return (<div className="rowtop">
+    <PeachBox />
+    <PeachBox1 />
+    <PeachBox />
+  </div>
+  )
+}
+function PeachBox() {
+  return (
+    <div className="peach-box">
+    </div>
+  )
+}
+function PeachBox1() {
+  return (
+    <div className="peach-box">
+      <RedBox />
+    </div>
+  )
+}
+function RedBox() {
+  return (
+    <div className="red-box">
+      <PinkLeft name="Stephany" />
+      <PinkRight name="Dela Pena" />
+    </div>
+  )
+}
+function PinkLeft(props) {
+  return (
+    <div className="pink-left">{props.name}
+    </div>
+  )
+}
+function PinkRight(props) {
+  return (
+    <div className="pink-right">{props.name}
+    </div>
+  )
+}
+function BlueBoxBottom() {
+  return (
+    <div className="blue-box-bot">
+      <Row1 />
+    </div>
+  )
+}
+function Row1() {
+  return (<div className="rowbot">
+    <PurpleBox />
+    <SmallPeach text="C-PCIT9" />
+    <SmallPeach text="IT3A" />
+    <PurpleBox />
+  </div>
+  )
+}
+function PurpleBox() {
+  return (
+    <div className="purple-box">
+    </div>
+  )
+}
+function SmallPeach(props) {
+  return (
+    <div className="small-peach">{props.text}
+    </div>
+  )
+}
+function App() {
+  return (
+    <OuterGreen />
   );
 }
 
